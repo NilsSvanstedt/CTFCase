71a3a8c379ecacc421dc7d30d83249321b16dd57
NilsSvanstedt <nils.svanstedt@gmail.com>
2022-02-22 11:19:14 +0100

    Added input for username to prevent abuse.

--- main.py
+++ main.py
@@ -5,12 +5,15 @@ from geopy.geocoders import Nominatim
 # set API key using config file to prevent abuse
 api_key = config.api_key
 
+user_agent = input("Enter your username: ")
 # Connection to nominatim api for getting coordinates
-geolocator = Nominatim(user_agent="Nils Bengtsson")
+geolocator = Nominatim(user_agent=f"{user_agent}")
 
 
-def getweather(cityname):
-    location = geolocator.geocode(cityname)
+def getweather(city_name):
+    # get coordinates from city name
+    location = geolocator.geocode(city_name)
+
     # set siteurl (current weather data api)
     try:
         return f'https://api.openweathermap.org/data/2.5/weather?lat={location.latitude}&lon={location.longitude}&units=Metric&appid={api_key}'
@@ -25,17 +28,18 @@ headers = {'Accept': 'application/vnd.github.v3+json'}
 # Input from user
 print("Type 0 to quit.")
 while 1 == 1:
-    cityname = input("Enter city name for weather data: ")
-    if cityname != '0':
-        if not cityname.isalpha():
+    city_name = input("Enter city name for weather data: ")
+    if city_name != '0':
+        if not city_name.isalpha():
             print("Input should solely consist of letters. Try again.")
             continue
         # call API and save response
-        url = getweather(cityname)
+        url = getweather(city_name)
         if url != 'error':
             response = requests.get(url, headers=headers)
             response_json = response.json()
-            print(f"Temperature: {response_json['main']['temp']} Celsius \nPressure: {response_json['main']['pressure']} kPa ")
+            print(
+                f"Temperature: {response_json['main']['temp']} Celsius \nPressure: {response_json['main']['pressure']} kPa ")
             print(f"Description: {response_json['weather'][0]['description']}")
     else:
         quit()
