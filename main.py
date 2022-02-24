8e78ff4979ba6567f392417bbac49d5d8fe8c7b8
NilsSvanstedt <nils.svanstedt@gmail.com>
2022-02-24 21:33:36 +0100

    added longer wait before timeout during nominatim API call, changed while 1==1 to while True

--- main.py
+++ main.py
@@ -1,20 +1,21 @@
 import requests  # HTTP library for easier HTTP requests
 import config  # file with API key
-from geopy.geocoders import Nominatim
+from geopy.geocoders import Nominatim  # API for finding coordinates
 
 # set API key using config file to prevent abuse
 api_key = config.api_key
 
 user_agent = input("Enter your username: ")
+
 # Connection to nominatim api for getting coordinates
 geolocator = Nominatim(user_agent=f"{user_agent}")
 
 
 def getweather(city_name):
     # get coordinates from city name
-    location = geolocator.geocode(city_name)
+    location = geolocator.geocode(city_name, timeout=10)
 
-    # set siteurl (current weather data api)
+    # set site url (current weather data api)
     try:
         return f'https://api.openweathermap.org/data/2.5/weather?lat={location.latitude}&lon={location.longitude}&units=Metric&appid={api_key}'
     except AttributeError:
@@ -22,12 +23,11 @@ def getweather(city_name):
         return 'error'
 
 
-# set the header
+# set the header to receive json data
 headers = {'Accept': 'application/vnd.github.v3+json'}
 
-# Input from user
 print("Type 0 to quit.")
-while 1 == 1:
+while True:
     city_name = input("Enter city name for weather data: ")
     if city_name != '0':
         if not city_name.isalpha():
